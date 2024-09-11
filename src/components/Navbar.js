8f0dc5e368f565bbd45b5a9fe3debac375695d32
Yasar070 <mdyasar682@gmail.com>
2024-09-11 19:41:33 +0530

    update project

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+function Navbar(){
+    return(
+     
+     
+  
+    
+   <div className="navbar">
+      <h1 >Perfumy</h1>
+  <div className="Listtag" >
+    <ul  style={{listStyleType:"none",}} className="Nav">
+   <li style={{color:"gray",margin:"2px"}}> Home |</li> 
+<li style={{color:"gray",margin:"2px",}} >Products |</li>
+ <li  style={{color:"gray",margin:"2px",}}>About |</li>
+   <li style={{color:"gray",margin:"2px",}}>Contact</li>
+    </ul>
+
+      </div>
+      
+   </div>
+         
+        
+        
+      
+
+    )
+  }
+
+  export default Navbar
